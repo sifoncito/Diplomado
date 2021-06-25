4dd9b1348ea90670a6fce99d6f6212da088d9e0b
aldo <aldoruelasinzunza@gmail.com>
2021-06-24 19:03:12 -0600

    Add unicode_cleaner

---
+++ unicode_cleaner.py
@@ -0,0 +1,34 @@
+import pandas as pd 
+import re
+import string
+
+
+text = pd.read_csv('verdesxsiempre_tweets.csv')
+
+
+def clean_text(text):
+    '''Make text lowercase, remove text in square brackets,remove links,remove punctuation
+    and remove words containing numbers.'''
+    text = str(text).lower()
+    text = re.sub('\[.*?\]', '', text)
+    text = re.sub('https?://\S+|www\.\S+', '', text)
+    text = re.sub('<.*?>+', '', text)
+    text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
+    text = re.sub('\n', '', text)
+    text = re.sub('\w*\d\w*', '', text)
+    return text
+
+def remove_emoji(text):
+    emoji_pattern = re.compile("["
+                           u"\U0001F600-\U0001F64F"  # emoticons
+                           u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                           u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                           u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                           u"\U00002702-\U000027B0"
+                           u"\U000024C2-\U0001F251"
+                           "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', text)
+
+text['text']=text['text'].apply(lambda x: remove_emoji(x))
+
+print(text)
